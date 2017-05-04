83c0d6169833d818946debc0710024a454980bd2
Hsuan Lee <boczeratul@gmail.com>
2017-05-05 00:27:00 +0800

    flatten export

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import message from './message';
-import service from './service';
 import Logger from './logger';
 import LOG_LEVEL from './enum/logLevel';
 
 export {
   LOG_LEVEL,
-  message,
-  service,
   Logger,
 };
+
+export * from './service';
+export * from './message';
