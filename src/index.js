7479f5c4981941619d5363a1f6219ed333231c34
Kai Hao <kevin830726@gmail.com>
2017-05-06 20:59:30 +0800

    try to make API better and easier to use

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Logger from './logger';
 import LOG_LEVEL from './enum/logLevel';
+import * as messages from './message';
+import methodAlias from './enum/methodAlias';
 
 export {
   LOG_LEVEL,
@@ -8,3 +10,32 @@ export {
 
 export * from './service';
 export * from './message';
+
+export default (config) => {
+  const internalLogger = new Logger(config);
+
+  return (label) => {
+    const labelledLogger = internalLogger.Label(label);
+
+    const logger = level => (...args) => {
+      let msg;
+
+      if (args.length === 2) {
+        msg = new messages.LogMessage(...args);
+      } else {
+        msg = new messages.ErrorMessage(...args);
+      }
+
+      labelledLogger.Log(
+        level,
+        msg,
+      );
+    };
+
+    methodAlias.forEach((method) => {
+      logger[method] = logger(methodAlias[method]);
+    });
+
+    return logger;
+  };
+};
