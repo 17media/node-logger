9220760e1716aeaf11798197ffc6ef8a9a385497
Kai Hao <kevin830726@gmail.com>
2017-05-06 20:59:30 +0800

    try to make API better and easier to use

---
+++ src/enum/methodAlias.js
@@ -0,0 +1,9 @@
+import LOG_LEVEL from './logLevel';
+
+export default {
+  error: LOG_LEVEL.ERROR,
+  warn: LOG_LEVEL.WARN,
+  info: LOG_LEVEL.INFO,
+  debug: LOG_LEVEL.DEBUG,
+  log: LOG_LEVEL.DEBUG,
+};
