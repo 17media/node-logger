458e15cc5c551ce582191db137e288cd5b7163fb
Hsuan Lee <boczeratul@gmail.com>
2017-05-08 03:57:44 +0800

    expose createLogger apart from default

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import createLogger from './createLogger';
 
+export { createLogger };
 export { default as Logger } from './logger';
 export { default as Level } from './enum/level';
 export * from './service';
