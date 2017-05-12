c4e321c9612684c9b9c9662ccf4b1ee2f8f11d6b
Hsuan Lee <boczeratul@gmail.com>
2017-05-12 10:48:24 +0800

    add corresponding tests

--- src/__tests__/createLogger.js
+++ src/__tests__/createLogger.js
@@ -52,6 +52,22 @@ describe('createLogger', () => {
     Console.prototype.IsConfigValid = originalConsoleConfigCheck;
   });
 
+  it('should wait for logger to finish in promise', (done) => {
+    // delay log services
+    const delay = new Promise(resolve => setTimeout(resolve, 1000));
+    const logFinished = jest.fn(() => Promise.resolve());
+    Logger.prototype.Log = jest.fn(() => delay.then(logFinished));
+    const logMessage = new LogMessage(message);
+
+    createLogger(config)(label)(Level.INFO)(logMessage)
+      .then(() => {
+        expect(Logger.prototype.Log).toHaveBeenCalledTimes(1);
+        expect(Logger.prototype.Log).toHaveBeenCalledWith(Level.INFO, logMessage, label);
+        expect(logFinished).toHaveBeenCalledTimes(1);
+        done();
+      });
+  });
+
   it('should preset label for future logs', () => {
     const logMessage = new LogMessage(message);
 
